b746217922874db0f31122b82ab8cdd9319ea597
Graham Taylor <gtayzlor@gmail.com>
2015-10-02 15:21:13 +0100

    Add sunburst component to nodes page to visualise node resources

--- src/pages/Nodes/Nodes.js
+++ src/pages/Nodes/Nodes.js
@@ -1,7 +1,13 @@
 import React, { PropTypes } from 'react';
 import PageTitle from '../../components/PageTitle';
+import DashboardBox from '../../components/DashboardBox';
+import Sunburst from '../../components/Sunburst';
 
 class Nodes extends React.Component {
+
+  static propTypes = {
+    nodes: PropTypes.array.isRequired
+  };
 
   static contextTypes = {
     onSetTitle: PropTypes.func.isRequired
@@ -9,11 +15,22 @@ class Nodes extends React.Component {
 
   render() {
     let title = 'Nodes';
+    let nodes = this.props.nodes;
+
+    let widgets = nodes.map(function(node, i){
+      return React.createElement(DashboardBox, { title: node.hostname },
+        React.createElement(Sunburst, { key: i, totalResources: node.resources, usedResources: node.usedResources })
+      );
+    });
+
     this.context.onSetTitle(title);
 
     return (
       <div>
         <PageTitle title={title} />
+        <div className="row">
+          { widgets }
+        </div>
       </div>
     );
   }
