82f6abb5a95ab1e24fd956964f43e7764b4a054f
Graham Taylor <gtayzlor@gmail.com>
2015-10-02 10:46:14 +0100

    Add DashboardBox component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,14 +4,21 @@ import withContext from '../../decorators/withContext';
 import mui, { FontIcon } from 'material-ui';
 import Logo from '../Logo';
 import Navigation from '../Navigation';
+import Radium from 'radium';
 
 let ThemeManager = new mui.Styles.ThemeManager();
 
 @withContext
+@Radium
 class App extends React.Component {
 
   static childContextTypes = {
     muiTheme: React.PropTypes.object
+  };
+
+  constructor() {
+    super();
+    this.handleResize = this.handleResize.bind(this);
   }
 
   getChildContext() {
@@ -21,7 +28,23 @@ class App extends React.Component {
   }
 
   componentWillMount() {
-    ThemeManager.setTheme(ThemeManager.types.DARK);
+   // ThemeManager.setTheme(ApolloTheme);
+  }
+
+  componentDidMount() {
+    window.addEventListener('resize', this.handleResize);
+    this.handleResize();
+  }
+
+  handleResize() {
+    let widthCurrent = window.matchMedia('(minWidth: 1024px)').matches;
+    if (this.state.widthMedium !== widthCurrent) {
+      this.setState({widthMedium: widthCurrent});
+    }
+  }
+
+  componentWillUnMount() {
+    window.removeEventListener('resize', this.handleResize);
   }
 
   menuItems() {
@@ -32,27 +55,58 @@ class App extends React.Component {
     let dashboardIcon = React.createElement(FontIcon, {style: iconStyle, className: 'material-icons'}, 'settings_input_svideo' );
     let nodesIcon = React.createElement(FontIcon, {style: iconStyle, className: 'material-icons'}, 'dns' );
     let frameworksIcon = React.createElement(FontIcon, {style: iconStyle, className: 'material-icons'}, 'build' );
+
+    let dashboardText = this.state.widthMedium ? 'Dashboard' : '';
+    let frameworksText = this.state.widthMedium ? 'Frameworks' : '';
+    let nodesText = this.state.widthMedium ? 'Nodes' : '';
+
     return [
-      { route: '/', text: 'Dashboard', icon: dashboardIcon },
-      { route: 'frameworks', text: 'Frameworks', icon: frameworksIcon },
-      { route: 'nodes', text: 'Nodes', icon: nodesIcon }
+      { route: '/', text: dashboardText, icon: dashboardIcon },
+      { route: 'frameworks', text: frameworksText, icon: frameworksIcon },
+      { route: 'nodes', text: nodesText, icon: nodesIcon }
     ];
   }
 
   getStyle() {
-    return {
-      padding: '24'
+    let style = {
+      sideBar: {
+        transition: 'flex 0.5s ease-out',
+        flex: '0 0 66px',
+        backgroundColor: '#20272b',
+        '@media (minWidth: 1024px)': {
+          flex: '0 0 170px'
+        }
+      },
+      columns: {
+        padding: '20px 0 0 10px',
+        '@media (minWidth: 1024px)': {
+          padding: '20px 24px 0'
+        }
+      },
+      logo: {
+        padding: '10px 12px'
+      }
     };
+    return style;
   }
 
   render() {
+    let style = this.getStyle();
     return (
       <div>
-        <div className="col-xs-2">
-          <Logo width={150} height={100} />
-          <Navigation menuItems={this.menuItems()} />
+        <div className="row">
+          <div style={style.sideBar}>
+            <div style={style.logo}>
+              <Logo width={50} height={50} />
+            </div>
+            <Navigation menuItems={this.menuItems()} />
+          </div>
+          <div className="col-xs-9 col-sm-10">
+            <div style={style.columns} className="col-xs-12">
+              <RouteHandler {...this.props} />
+            </div>
+          </div>
         </div>
-        <RouteHandler {...this.props} />
       </div>
     );
   }
---
+++ src/components/DashboardBox/DashboardBox.js
@@ -0,0 +1,76 @@
+import React, { PropTypes } from 'react';
+import { Card, CardTitle } from 'material-ui';
+import _ from 'lodash';
+
+class DashboardBox extends React.Component {
+  static propTypes = {
+    title: PropTypes.string,
+    styles: PropTypes.object,
+    children: PropTypes.object,
+    xsColSize: PropTypes.number,
+    smColSize: PropTypes.number,
+    mdColSize: PropTypes.number,
+    lgColSize: PropTypes.number
+  };
+
+  static defaultProps = {
+    xsColSize: 12,
+    smColSize: 6,
+    mdColSize: 4,
+    styles: {}
+  };
+
+  constructor(props) {
+    super(props);
+  }
+
+  getStyles() {
+    return {
+      root: {
+        borderRadius: 5,
+        minHeight: 315
+      },
+      card: {
+        titleColor: '#ffffff',
+        subtitleColor: '#ffffff'
+      }
+    };
+  }
+
+  getCols() {
+    let xs = this.props.xsColSize;
+    let sm = this.props.smColSize;
+    let md = this.props.mdColSize;
+    let lg = this.props.lgColSize;
+
+    let cols = 'col-xs-' + xs;
+    if (sm) {
+      cols = cols + ' col-sm-' + sm;
+    }
+    if (md) {
+      cols = cols + ' col-md-' + md;
+    }
+    if (lg) {
+      cols = cols + ' col-lg-' + lg;
+    }
+    return cols;
+  }
+
+  render() {
+    let styles = _.merge(this.getStyles(), this.props.styles);
+
+    return (
+      <div className={this.getCols()}>
+        <div className="box">
+          <Card style={styles.root}>
+            <CardTitle subtitle={this.props.title} titleColor={styles.card.titleColor} subtitleColor={styles.card.subtitleColor} />
+            {this.props.children}
+          </Card>
+        </div>
+      </div>
+    );
+  }
+
+}
+
+export default DashboardBox;
